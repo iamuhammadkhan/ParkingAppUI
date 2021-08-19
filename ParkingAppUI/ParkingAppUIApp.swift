77bf89a83a95bbdded7a4e8d7da145fbb0a6f048
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-19 15:27:04 +0500

    Initial Commit

---
+++ ParkingAppUI/ParkingAppUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingAppUIApp.swift
+//  ParkingAppUI
+//
+//  Created by Muhammad Khan on 8/19/21.
+//
+
+import SwiftUI
+
+@main
+struct ParkingAppUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
